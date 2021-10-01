ea256c4e73a9d961a4909a392b5290b91fb06d07
katieorlova <orl-ka@yandex.ru>
2021-10-01 19:52:49 +0300

    commit2

---
+++ 2.py
@@ -0,0 +1,7 @@
+katet1 = int (input ('Vvedite perviy katet: '))
+katet2 = int (input ('Vvedite vtoroy katet: '))
+gipotenuza_v_kvadrate = katet1**2+katet2**2
+gipotenuza = gipotenuza_v_kvadrate**0.5
+
+print ('S = ', katet1*katet2/2, sep = " ")
+print ('P = ', katet1+katet2+gipotenuza, sep = " ")
