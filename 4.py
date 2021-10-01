bebcd08552bad21c58bad7bca4ef12526aa60bfe
katieorlova <orl-ka@yandex.ru>
2021-10-01 20:13:08 +0300

    commit4

---
+++ 4.py
@@ -0,0 +1,5 @@
+t = int (input ('Vvedite kolichestvo let: '))
+k = int (input ('Vvedite kolichestvo ecsponatov: '))
+
+print ('Kolichestvo eksponatov = ', t*365*8*60/5)
+print ('Kolichestvo minut = ', k*5)
